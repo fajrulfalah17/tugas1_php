97e02542017f17d6e17b781d10fad998b2dadba1
fajrulfalah17 <fajruljunior45@gmail.com>
2021-09-22 22:22:42 +0700

    Tugas 1 PHP
    tugas okje

---
+++ tugas1_2.php
@@ -0,0 +1,39 @@
+<?php
+$border = "style='border: 1px solid black'";
+
+echo "<h1>Daftar Menu</h1>";
+echo "<table style='border: 1px solid black'>
+<tr table style='border: 1px solid black'>
+<th $border>No</th>
+<th $border>Nama Menu</th>
+<th $border>Harga</th>
+</tr>
+<tr>
+<td $border>1</td>
+<td $border>Cireng</td>
+<td $border>Rp. 1000</td>
+</tr>
+<tr>
+<td $border>2</td>
+<td $border>Es Duren</td>
+<td $border>Rp. 15000</td>
+</tr>
+<tr>
+<td $border>3</td>
+<td $border>Es Milo</td>
+<td $border>Rp. 20000</td>
+</tr>
+<tr>
+<td $border>4</td>
+<td $border>Es Teh</td>
+<td $border>Rp. 5000</td>
+</tr>
+<tr>
+<td $border>5</td>
+<td $border>Piscok</td>
+<td $border>Rp. 1000</td>
+</tr>
+
+</table>"
+
+?>
---
+++ tugas_1.php
@@ -0,0 +1,17 @@
+<?php
+$rupiah = "Rp. ";
+
+echo "<h1>Daftar Menu</h1>";
+echo "<ul>
+<li>Ayam Goreng = $rupiah 13000</li>
+<li>Ayam Bakar = $rupiah 15000</li>
+<li>Ayam Sayur = $rupiah 13000</li>
+<li>Tempe Goreng = $rupiah 1000</li>
+<li>Tahu Goreng = $rupiah 1000</li>
+<li>Soto = $rupiah 3000</li>
+<li>Nasi Putih = $rupiah 5000</li>
+<li>Es Teh = $rupiah 4000</li>
+<li>Es Tebu = $rupiah 5000</li>
+</ul>"
+
+?>
